962048878e10740da88a0af1748db2c2a14f0b93
iTrooz_ <team.mortelgames@gmail.com>
2020-08-01 15:34:54 +0200

    upgrade menu improve

--- src/fr/entasia/skycore/objs/isutils/Extensions.java
+++ src/fr/entasia/skycore/objs/isutils/Extensions.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public enum Extensions {
-	X100(11, "§7100x100 (par défaut)", Material.IRON_BLOCK, 0, 0, "§7La taille par défaut de ton île. C'est quand même assez grand pour commencer !"),
-	X200(12, "§3200x200", Material.LAPIS_BLOCK, 100, 100, "§3Tu devrais pouvoir faire des grands champs et de bonnes récoltes avec ca !"),
-	X300(14, "§6300x300", Material.GOLD_BLOCK, 250, 250, "§6Une bonne taille pour faire de belles villes flottantes !"),
-	X400(15, "§b400x400", Material.DIAMOND_BLOCK, 500, 500, "§bJe vois pas à quoi ca pourrait te servir mais bon, tu pourra te la péter !");
+	X100(20, "§7100x100 (par défaut)", Material.IRON_BLOCK, 0, 0, "§7La taille par défaut de ton île. C'est quand même assez grand pour commencer !"),
+	X200(21, "§3200x200", Material.LAPIS_BLOCK, 100, 100, "§3Tu devrais pouvoir faire des grands champs et de bonnes récoltes avec ca !"),
+	X300(23, "§6300x300", Material.GOLD_BLOCK, 250, 250, "§6Une bonne taille pour faire de belles villes flottantes !"),
+	X400(24, "§b400x400", Material.DIAMOND_BLOCK, 500, 500, "§bJe vois pas à quoi ca pourrait te servir mais bon, tu pourra te la péter !");
 
 	public int slot;
 	public String name;
